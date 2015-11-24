42e483250c9194689dc42d604a55b710cfaf7a96
Mogria <m0gr14@gmail.com>
2015-11-24 18:49:59 +0100

    tools installed by composer can now be properly

--- toolscript.sh
+++ toolscript.sh
@@ -4,8 +4,9 @@ if [ "$1" == "composer" ]; then
     shift
     exec composer "$@"
 elif [ -f "/data/www/vendor/bin/$1" ]; then
+    executable="$1" # save executable name because of shift
     shift
-    exec "/data/www/vendor/bin/$2" "$@" # is $2 because of shift
+    exec "/data/www/vendor/bin/$executable" "$@"
 fi
 
 exec "composer" "$@"
